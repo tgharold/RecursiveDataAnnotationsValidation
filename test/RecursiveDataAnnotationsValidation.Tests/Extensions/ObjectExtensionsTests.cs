1e82407f712d13db25af57ce0e3a981e175f4cfd
Thomas Harold <tgharold@gmail.com>
2020-03-09 12:06:41 -0400

    clean up the ObjectExtensionsTests

--- test/RecursiveDataAnnotationsValidation.Tests/Extensions/ObjectExtensionsTests.cs
+++ test/RecursiveDataAnnotationsValidation.Tests/Extensions/ObjectExtensionsTests.cs
@@ -14,11 +14,35 @@ namespace RecursiveDataAnnotationsValidation.Tests.Extensions
         }
         
         [Fact]
-        public void Can_handle_property_name_onnull_object()
+        public void Can_handle_property_name_on_null_object()
         {
             object sut = null;
             var result = sut.GetPropertyValue("someProperty");
             Assert.Null(result);
+        }
+
+        [Fact]
+        public void Get_null_for_bogus_property_name()
+        {
+            var sut = new { PropA = 5 };
+            var result = sut.GetPropertyValue("doesNotExist");
+            Assert.Null(result);
+        }
+        
+        [Fact]
+        public void Get_result_for_int_property()
+        {
+            var sut = new { PropA = 5 };
+            var result = sut.GetPropertyValue(nameof(sut.PropA));
+            Assert.Equal(sut.PropA, result);
+        }
+        
+        [Fact]
+        public void Get_result_for_string_property()
+        {
+            var sut = new { PropA = "XYZ" };
+            var result = sut.GetPropertyValue(nameof(sut.PropA));
+            Assert.Equal(sut.PropA, result);
         }
     }
 }
