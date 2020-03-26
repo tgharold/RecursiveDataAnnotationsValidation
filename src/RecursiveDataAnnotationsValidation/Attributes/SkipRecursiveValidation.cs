e2e5917674d95e8e5d386abb13c9a637a9ab81c2
Thomas Harold <tgharold@gmail.com>
2020-03-26 14:24:33 -0400

    Add XML documentation to methods/classes

--- src/RecursiveDataAnnotationsValidation/Attributes/SkipRecursiveValidation.cs
+++ src/RecursiveDataAnnotationsValidation/Attributes/SkipRecursiveValidation.cs
@@ -2,6 +2,8 @@ using System;
 
 namespace RecursiveDataAnnotationsValidation.Attributes
 {
+    /// <summary>Used to decorate child object properties where you do not want the
+    /// child object to be validated by the recursive validation.</summary>
     [AttributeUsage(AttributeTargets.Property)]
     public class SkipRecursiveValidationAttribute : Attribute
     {
